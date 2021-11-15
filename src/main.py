d7048c4cc657f318e274057f95a1e61ccdade4cc
Folkenciyo <folkencillo@gmail.com>
2021-11-15 22:43:07 +0000

    primeros pasitos para decir que me queda poco y lo he entendido

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Task
+from sqlalchemy import exc
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +31,57 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/task', methods=['GET'])
+def get_task():
+    tasks= Task.get_all()
+    all_tasks=[task.to_dict() for task in tasks]
+    return jsonify(all_tasks), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/task/<int:id>', methods=['GET'])
+def get_task_by_id(id):
+    task= Task.get_by_id(id)
 
-    return jsonify(response_body), 200
+    if task:
+        return jsonify(task.to_dict()), 200
+
+        return jsonify({'error': 'task not found'}), 404
+
+@app.route('/task', methods=['POST'])
+def create_item():
+    new_item=request.json.get('item',None)
+
+    if not new_item:
+        return jsonify({'error':'missing items'}), 400
+
+    task= Task(item=new_item, done=False)
+    try:
+        task_created=task.create()
+        return jsonify(task_created.to_dict()), 201
+    except exc.IntegrityError:
+        return jsonify({'error': 'fail in data'}), 400
+
+@app.route('/task/<int:id>', methods=['PUT','PATCH'])
+def update_task(id):
+    new_item=request.json.get('item', None)
+
+    if not new_item:
+        return jsonify({'error': 'missing items'}), 400
+
+    task= Task.get_by_id(id)
+    if task:
+        task=task.update(new_item)
+        return jsonify(task.to_dict()), 200
+
+    return jsonify({'error': 'task not found'}), 404
+
+@app.route('/task/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task=Task.get_by_id(id)
+    if task:
+        task.delete()
+        return jsonify(task.to_dict()), 200
+
+    return jsonify({'error':'task not found'}), 404
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    item = db.Column(db.String(250), unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def repr(self):
+        return f'Task is :{self.item}, done:{self.done}, id:{self.id}'
 
-    def serialize(self):
+    def to_dict(self):
+
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "item": self.item,
+            "done": self.done
+
         }
+
+    @classmethod
+    def get_by_id(cls,id_task):
+        task= cls.query.filter_by(id=id_task).one_or_none()
+        return task
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def get_all(cls):
+        tasks= cls.query.all()
+        return tasks
+
+    def update(self, item):
+        self.item=item
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
